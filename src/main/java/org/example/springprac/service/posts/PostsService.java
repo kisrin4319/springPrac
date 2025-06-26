266bb8fc7ef7c79e03a81a69567abaf155e68e6b
kisrin4319 <kisrin4319@gmail.com>
2025-06-27 01:02:18 +0900

    Post 수정 API 및 BaseEntity 까지 구현

--- src/main/java/org/example/springprac/service/posts/PostsService.java
+++ src/main/java/org/example/springprac/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package org.example.springprac.service.posts;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springprac.domain.posts.Posts;
 import org.example.springprac.domain.posts.PostsRepository;
+import org.example.springprac.web.dto.PostsResponseDto;
 import org.example.springprac.web.dto.PostsSaveRequestDto;
+import org.example.springprac.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,5 +18,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
