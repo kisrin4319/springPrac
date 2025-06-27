0c8ccf8b14e3ae16941fb10e648410a1fd994a58
kisrin4319 <kisrin4319@gmail.com>
2025-06-27 20:55:51 +0900

    Chapter4. 머스테치로 화면 구성하기

--- src/main/java/org/example/springprac/service/posts/PostsService.java
+++ src/main/java/org/example/springprac/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package org.example.springprac.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.springprac.domain.posts.Posts;
 import org.example.springprac.domain.posts.PostsRepository;
+import org.example.springprac.web.dto.PostsListResponseDto;
 import org.example.springprac.web.dto.PostsResponseDto;
 import org.example.springprac.web.dto.PostsSaveRequestDto;
 import org.example.springprac.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +41,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                        .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.deleteById(id);
     }
 }
